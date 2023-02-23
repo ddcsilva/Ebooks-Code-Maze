07c0721fa4a0e13ebc628aa8842b8197923c5c46
Danilo Silva <danilo.silva@msn.com>
2023-02-23 07:59:16 -0300

    Trabalhando com Map e MapWhen

--- Ultimate-ASP-NET-Core-Web-API/MiddlewareExample/MiddlewareExample/Program.cs
+++ Ultimate-ASP-NET-Core-Web-API/MiddlewareExample/MiddlewareExample/Program.cs
@@ -19,6 +19,30 @@ app.Use(async (context, next) =>
     Console.WriteLine($"Logica depois de executar o delegate Next no metodo Use");
 });
 
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("A logica do Map no método Use antes do próximo delegate");
+        await next.Invoke();
+        Console.WriteLine("A logica do Map no método Use depois do próximo delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"A resposta do Map para o cliente no método Run");
+        await context.Response.WriteAsync("Saudacoes do Map");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder
+=>
+{
+    builder.Run(async context =>
+    {
+        await context.Response.WriteAsync("Saudacoes do MapWhen");
+    });
+});
+
 app.Run(async context =>
 {
     Console.WriteLine($"Exibindo a resposta para o cliente no metodo Run");
