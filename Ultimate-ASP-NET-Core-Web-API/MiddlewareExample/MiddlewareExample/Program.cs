91b228bce2c54d86c52e782b8bdb35a89d6dc71c
Danilo Silva <danilo.silva@msn.com>
2023-02-23 07:42:39 -0300

    Trabalhando com o método Use

--- Ultimate-ASP-NET-Core-Web-API/MiddlewareExample/MiddlewareExample/Program.cs
+++ Ultimate-ASP-NET-Core-Web-API/MiddlewareExample/MiddlewareExample/Program.cs
@@ -12,8 +12,16 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logica antes de executar o delegate Next no metodo Use");
+    await next.Invoke();
+    Console.WriteLine($"Logica depois de executar o delegate Next no metodo Use");
+});
+
 app.Run(async context =>
 {
+    Console.WriteLine($"Exibindo a resposta para o cliente no metodo Run");
     await context.Response.WriteAsync("Componente Middleware dizendo um Oi!");
 });
 
