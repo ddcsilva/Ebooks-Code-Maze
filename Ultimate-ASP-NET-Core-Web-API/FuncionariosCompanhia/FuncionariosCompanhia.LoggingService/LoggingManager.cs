2c62efd64316de8268d0fbc2d5d4f0853326b3f6
Danilo Silva <danilo.silva@msn.com>
2023-03-04 20:28:18 -0300

    Implementando a Interface e o Arquivo Nlog.Config

---
+++ Ultimate-ASP-NET-Core-Web-API/FuncionariosCompanhia/FuncionariosCompanhia.LoggingService/LoggingManager.cs
@@ -0,0 +1,19 @@
+﻿using FuncionariosCompanhia.Contracts;
+using NLog;
+
+namespace FuncionariosCompanhia.LoggingService;
+
+public class LoggingManager : ILoggerManager
+{
+    private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+    public LoggingManager() {}
+
+    public void LogAdvertencia(string messagem) => logger.Warn(messagem);
+
+    public void LogDepuracao(string messagem) => logger.Debug(messagem);
+
+    public void LogErro(string messagem) => logger.Error(messagem);
+
+    public void LogInformativo(string messagem) => logger.Info(messagem);
+}
