546ad1b7c71f456e3d02505e79f5f828d4b7c6ad
Danilo Silva <danilo.silva@msn.com>
2023-03-06 11:48:10 -0300

    Classe de contexto e a conexão com o banco de dados

---
+++ Ultimate-ASP-NET-Core-Web-API/FuncionariosCompanhia/FuncionariosCompanhia.Repository/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using FuncionariosCompanhia.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosCompanhia.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Companhia>? Companhias { get; set; }
+    public DbSet<Funcionario>? Funcionarios { get; set; }
+}
